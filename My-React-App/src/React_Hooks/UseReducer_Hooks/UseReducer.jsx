7546e0174a334858471756729cb789aa4c401161
Noyon-Tech-Solution <shahadathosen8987@gmail.com>
2024-11-30 21:18:00 +0600

    Noyon Tech Solution

--- My-React-App/src/React_Hooks/UseReducer_Hooks/UseReducer.jsx
+++ My-React-App/src/React_Hooks/UseReducer_Hooks/UseReducer.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useReducer } from "react";
+
+const initialState = 0;
+const reducer = (state, action) => {
+  switch (action) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    default:
+      return state;
+  }
+};
 
 export default function UseReducer() {
+  const [count, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div>
-      <h1>I am useReducer</h1>
+      <h1>Count : {count}</h1>
+      <button onClick={() => dispatch("increment")}>Increment</button>
+      <button onClick={() => dispatch("decrement")}>Decrement</button>
     </div>
   );
 }
